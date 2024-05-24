25918cd340aa462e5f9d58ecfa1e6f6b6aadaf07
Thibault Friedrich <thibault.friedrich@gmail.com>
2024-05-24 07:58:57 -0400

    ci: fix jest test

--- packages/documentation-portal/src/providers/figma/components/figma-viewer.tsx
+++ packages/documentation-portal/src/providers/figma/components/figma-viewer.tsx
@@ -1,3 +1,4 @@
+import SvgGrid from '@/components/atoms/svg-grid'
 import Component, {
   ComponentVariant,
 } from '@/features/component/types/Component'
@@ -17,45 +18,7 @@ function FigmaViewer({ component }: FigmaViewerProps) {
 
   return (
     <div className="relative min-h-20 p-4 flex items-center justify-center">
-      <svg
-        width="100%"
-        height="100%"
-        xmlns="http://www.w3.org/2000/svg"
-        className="absolute inset-0 z-0"
-      >
-        <defs>
-          <pattern
-            id="smallGrid"
-            width="10"
-            height="10"
-            patternUnits="userSpaceOnUse"
-          >
-            <path
-              d="M 10 0 L 0 0 0 10"
-              fill="none"
-              stroke="lightblue"
-              stroke-width="0.5"
-              className="stroke-slate-300 dark:stroke-slate-700"
-            />
-          </pattern>
-          <pattern
-            id="grid"
-            width="100"
-            height="100"
-            patternUnits="userSpaceOnUse"
-          >
-            <rect width="100" height="100" fill="url(#smallGrid)" />
-            <path
-              d="M 100 0 L 0 0 0 100"
-              fill="none"
-              className="stroke-slate-300 dark:stroke-slate-700"
-              stroke-width="1"
-            />
-          </pattern>
-        </defs>
-
-        <rect width="100%" height="100%" fill="url(#grid)" />
-      </svg>
+      <SvgGrid />
       <Image
         className="relative z-20"
         src={component.thumbnailUrl?.figma ?? ''}

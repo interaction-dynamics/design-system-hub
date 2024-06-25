3a21802508ac16cbc4bd19240fb787b8f6777155
Thibault Friedrich <thibault.friedrich@gmail.com>
2024-06-25 14:46:16 -0400

    feat: install google analytics and hotjar

--- packages/documentation-portal/src/app/layout.tsx
+++ packages/documentation-portal/src/app/layout.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { GoogleAnalytics } from '@next/third-parties/google'
+import Script from 'next/script'
+
 import './globals.css'
 import ThemeProvider from '@/components/organisms/theme-provider'
 import { cn } from '@/lib/utils'
@@ -29,6 +32,17 @@ export default function RootLayout({
           {children}
         </ThemeProvider>
       </body>
+      <GoogleAnalytics gaId={process.env.GOOGLE_TAG_ID ?? ''} />
+      <Script id="hotjar">
+        {`(function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:${process.env.HOTJAR_ID ?? ''},hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`}
+      </Script>
     </html>
   )
 }

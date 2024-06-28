ba9c3299904c851f846fb1b200c1767427c5bf51
Thibault Friedrich <thibault.friedrich@gmail.com>
2024-06-28 07:52:36 -0400

    feat: add icons in navigation

--- packages/documentation-portal/src/app/[designSystemSlug]/_components/navigation.tsx
+++ packages/documentation-portal/src/app/[designSystemSlug]/_components/navigation.tsx
@@ -6,14 +6,72 @@ import {
   NavigationMenuList,
   navigationMenuTriggerStyle,
 } from '@/components/ui/navigation-menu'
+import { cn } from '@/lib/utils'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 
 const navigation = [
   // { title: 'Getting Started', href: '/' },
-  // { title: 'Principles', href: 'principles' },
-  { title: 'Styles', href: 'styles' },
-  { title: 'Components', href: 'components' },
+  // {
+  //   title: 'Principles',
+  //   href: 'principles',
+  //   icon: (
+  //     <svg
+  //       xmlns="http://www.w3.org/2000/svg"
+  //       fill="none"
+  //       viewBox="0 0 24 24"
+  //       strokeWidth={1.5}
+  //       stroke="currentColor"
+  //       className="size-5"
+  //     >
+  //       <path
+  //         strokeLinecap="round"
+  //         strokeLinejoin="round"
+  //         d="M12 21v-8.25M15.75 21v-8.25M8.25 21v-8.25M3 9l9-6 9 6m-1.5 12V10.332A48.36 48.36 0 0 0 12 9.75c-2.551 0-5.056.2-7.5.582V21M3 21h18M12 6.75h.008v.008H12V6.75Z"
+  //       />
+  //     </svg>
+  //   ),
+  // },
+  {
+    title: 'Styles',
+    href: 'styles',
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="size-5"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M9.53 16.122a3 3 0 0 0-5.78 1.128 2.25 2.25 0 0 1-2.4 2.245 4.5 4.5 0 0 0 8.4-2.245c0-.399-.078-.78-.22-1.128Zm0 0a15.998 15.998 0 0 0 3.388-1.62m-5.043-.025a15.994 15.994 0 0 1 1.622-3.395m3.42 3.42a15.995 15.995 0 0 0 4.764-4.648l3.876-5.814a1.151 1.151 0 0 0-1.597-1.597L14.146 6.32a15.996 15.996 0 0 0-4.649 4.763m3.42 3.42a6.776 6.776 0 0 0-3.42-3.42"
+        />
+      </svg>
+    ),
+  },
+  {
+    title: 'Components',
+    href: 'components',
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        className="size-5"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M2.25 7.125C2.25 6.504 2.754 6 3.375 6h6c.621 0 1.125.504 1.125 1.125v3.75c0 .621-.504 1.125-1.125 1.125h-6a1.125 1.125 0 0 1-1.125-1.125v-3.75ZM14.25 8.625c0-.621.504-1.125 1.125-1.125h5.25c.621 0 1.125.504 1.125 1.125v8.25c0 .621-.504 1.125-1.125 1.125h-5.25a1.125 1.125 0 0 1-1.125-1.125v-8.25ZM3.75 16.125c0-.621.504-1.125 1.125-1.125h5.25c.621 0 1.125.504 1.125 1.125v2.25c0 .621-.504 1.125-1.125 1.125h-5.25a1.125 1.125 0 0 1-1.125-1.125v-2.25Z"
+        />
+      </svg>
+    ),
+  },
 ]
 
 export interface NavigationProps {
@@ -25,14 +83,18 @@ export default function Navigation({ designSystemSlug }: NavigationProps) {
 
   return (
     <NavigationMenu>
-      <NavigationMenuList>
-        {navigation.map(({ title, href }) => (
+      <NavigationMenuList className="gap-5">
+        {navigation.map(({ title, href, icon }) => (
           <NavigationMenuItem key={href}>
             <Link href={`/${designSystemSlug}/${href}`} legacyBehavior passHref>
               <NavigationMenuLink
-                className={navigationMenuTriggerStyle()}
+                className={cn(
+                  navigationMenuTriggerStyle(),
+                  'flex items-center gap-2'
+                )}
                 active={pathname.includes(href)}
               >
+                {icon}
                 {title}
               </NavigationMenuLink>
             </Link>

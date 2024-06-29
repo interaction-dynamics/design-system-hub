1f7215686b20918785f9613696b84374a5f41d18
Thibault Friedrich <thibault.friedrich@gmail.com>
2024-06-28 21:47:55 -0400

    feat: add log

--- packages/documentation-portal/src/app/(private)/design-system/new/figma/callback/page.tsx
+++ packages/documentation-portal/src/app/(private)/design-system/new/figma/callback/page.tsx
@@ -12,7 +12,7 @@ export default async function Page({
 
   return (
     <div className="fixed inset-0 bg-backgroun z-50 flex text-foreground items-center justify-center">
-      Done
+      {JSON.stringify(response)}
     </div>
   )
 }

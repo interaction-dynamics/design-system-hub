db417a07ba8e88fb108c0a3c7f5851523a51b519
Thibault Friedrich <thibault.friedrich@gmail.com>
2024-07-31 18:17:21 -0400

    fix: upload size

--- packages/documentation-portal/src/adapters/storage/assets.ts
+++ packages/documentation-portal/src/adapters/storage/assets.ts
@@ -1,8 +1,6 @@
-import {
-  S3Client,
-  PutObjectCommand,
-  UploadPartCommand,
-} from '@aws-sdk/client-s3'
+import { S3Client } from '@aws-sdk/client-s3'
+import { Upload } from '@aws-sdk/lib-storage'
+
 import crypto from 'node:crypto'
 
 const client = new S3Client({
@@ -16,19 +14,36 @@ const client = new S3Client({
 export const copyAsset = async (source: string): Promise<string> => {
   const id = crypto.randomBytes(16).toString('hex')
 
-  const body = await fetch(source).then((res) => res.text())
+  const body = await fetch(source).then((res) => res.body)
 
   if (!body) return ''
 
   const newKey = `documentation-portal/components-assets/${id}.webp`
 
-  const command = new PutObjectCommand({
-    Bucket: process.env.AWS_S3_BUCKET_NAME,
-    Key: newKey,
-    Body: body,
+  const parallelUploads3 = new Upload({
+    client,
+    params: {
+      Bucket: process.env.AWS_S3_BUCKET_NAME,
+      Key: newKey,
+      Body: body,
+      ContentType: 'image/webp',
+    },
+
+    // additional optional fields show default values below:
+
+    // (optional) concurrency configuration
+    queueSize: 4,
+
+    // (optional) size of each part, in bytes, at least 5MB
+    partSize: 1024 * 1024 * 5,
+
+    // (optional) when true, do not automatically call AbortMultipartUpload when
+    // a multipart upload fails to complete. You should then manually handle
+    // the leftover parts.
+    leavePartsOnError: false,
   })
 
-  const response = await client.send(command)
+  await parallelUploads3.done()
 
   const url = `https://${process.env.AWS_S3_BUCKET_NAME}.s3.${process.env.AWS_S3_REGION}.amazonaws.com/${newKey}`
   return url

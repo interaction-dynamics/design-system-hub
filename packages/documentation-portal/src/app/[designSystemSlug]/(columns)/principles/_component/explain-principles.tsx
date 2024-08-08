7ae39e8c450947d05aa192542445ee2828f67df2
Thibault Friedrich <thibault.friedrich@gmail.com>
2024-08-08 14:54:08 -0400

    fix: principles

--- packages/documentation-portal/src/app/[designSystemSlug]/(columns)/principles/_component/explain-principles.tsx
+++ packages/documentation-portal/src/app/[designSystemSlug]/(columns)/principles/_component/explain-principles.tsx
@@ -12,7 +12,7 @@ export function ExplainPrinciples({ designSystemSlug }: Props) {
 
       <Typography variant="p">
         In order to declare principles you should add markdown files to the
-        folder `docs/design-system/guidelines` in your code repository, then you
+        folder `docs/design-system/principles` in your code repository, then you
         can trigger the synchronization process again in the{' '}
         <Link
           className="underline hover:text-foreground"

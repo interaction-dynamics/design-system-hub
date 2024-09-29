b03414b84ee7408026b9e2d97ebe386aeee9d614
Thibault Friedrich <thibault.friedrich@gmail.com>
2024-09-29 07:41:41 -0400

    fix: github link

--- apps/documentation-portal/src/adapters/providers/code/utils/get-links.ts
+++ apps/documentation-portal/src/adapters/providers/code/utils/get-links.ts
@@ -4,7 +4,7 @@ import { CodeComponent } from '../types/code-component'
 const github = {
   name: 'GitHub',
   match: (url: string) =>
-    url.startsWith('git@github.com') || url.startsWith('https://github.com'),
+    url.startsWith('git@github.com') || url.startsWith('https://github.com/'),
   getLink: (path: string, designSystem: DesignSystem) => {
     const url = designSystem.providers.code.url
 
@@ -20,7 +20,8 @@ const github = {
         return `https://github.com/${owner}/${repoWithoutGit}/blob/main/${designSystem.providers.code.relativePath}/${path}`
       }
     } else if (url.startsWith('https://')) {
-      return `${url}/blob/main/${designSystem.providers.code.relativePath}/${path}`
+      const repositoryPath = url.replace(/\.git$/, '')
+      return `${repositoryPath}/blob/main/${designSystem.providers.code.relativePath}/${path}`
     }
 
     return ''
